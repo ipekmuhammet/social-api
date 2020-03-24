75929db327b9c767e7a472aaf8f7a63a5aa8716a
ipekmuhammet <muhammetipek57@hotmail.com>
2020-03-24 22:36:35 +0300

    init get orders by id

--- src/controllers/manager.ts
+++ src/controllers/manager.ts
@@ -31,4 +31,31 @@ router.get('/orders', (req, res) => {
 	])
 })
 
+router.get('/orders/:id', (req, res) => {
+	res.json({
+		id: 1,
+		customer: 'Muhammet İpek',
+		address: 'Ayvasaray Mah. Ahmet Rufai sok. No : 6/1',
+		date: new Date().toLocaleString(),
+		// starts : 2.2 // Müşteri daha önce memnuniyetsizliğini belirttiyse bi güzellik yapılabilir. :)
+		// price: (23.43 * 5) + (76.36 * 2), // Online ödemelerde manager'ın ücret ile işi yok.
+		products: [
+			{
+				Id: '1',
+				name: 'Alcoholic beverages',
+				price: '23.43',
+				categoryId: 0,
+				count: 5
+			},
+			{
+				Id: '12',
+				name: 'Danone Pro+ Muz & Yer Fıstıklı Proteinli Süt 330 Ml',
+				price: '76.36',
+				categoryId: 1,
+				count: 2
+			}
+		]
+	})
+})
+
 export default router
