42ac82c5a10ee15ac92252e74cdec969bf7f80de
ipekmuhammet <muhammetipek57@hotmail.com>
2020-03-25 12:51:34 +0300

    confirm cancel endpoint

--- src/controllers/manager.ts
+++ src/controllers/manager.ts
@@ -17,4 +17,28 @@ router.get('/orders/:id', (req, res) => {
 	})
 })
 
+router.put('/orders/cancel/:id', (req, res) => {
+	Redis.getInstance.hget('category1', req.params.id, (err0, reply0) => {
+		Redis.getInstance.hset('category1', req.params.id, JSON.stringify(Object.assign(JSON.parse(reply0), { status: false })), (err, reply) => {
+			if (err) {
+				res.json({ status: false })
+			} else {
+				res.json({ status: true })
+			}
+		})
+	})
+})
+
+router.put('/orders/confirm/:id', (req, res) => {
+	Redis.getInstance.hget('category1', req.params.id, (err0, reply0) => {
+		Redis.getInstance.hset('category1', req.params.id, JSON.stringify(Object.assign(JSON.parse(reply0), { status: true })), (err, reply) => {
+			if (err) {
+				res.json({ status: false })
+			} else {
+				res.json({ status: true })
+			}
+		})
+	})
+})
+
 export default router
