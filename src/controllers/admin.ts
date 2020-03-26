976d8574ef0f83ed09fa54b0796db3eb1897416b
ipekmuhammet <muhammetipek57@hotmail.com>
2020-03-26 05:47:59 +0300

    update product category

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -11,8 +11,24 @@ router.post('/saveCategory', (req, res) => {
 	})
 })
 
+router.put('/updateCategory/:id', (req, res) => {
+	Category.findOneAndUpdate({ id: req.params.id }, req.body).then((doc) => {
+		res.json(doc)
+	}).catch((reason) => {
+		res.json(reason)
+	})
+})
+
 router.post('/saveProduct', (req, res) => {
 	new Product(req.body).save().then((doc) => {
+		res.json(doc)
+	}).catch((reason) => {
+		res.json(reason)
+	})
+})
+
+router.put('/updateProduct/:id', (req, res) => {
+	Product.findOneAndUpdate({ id: req.params.id }, req.body).then((doc) => {
 		res.json(doc)
 	}).catch((reason) => {
 		res.json(reason)
