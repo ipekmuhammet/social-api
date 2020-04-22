5d6fc6f09b64b469e828ef1cfe84275b091032e4
ipekmuhammet <muhammetipek57@hotmail.com>
2020-04-22 12:26:15 +0300

    user service initialized

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,93 +1,67 @@
 import { Router } from 'express'
 import HttpStatusCodes from 'http-status-codes'
-import Iyzipay from 'iyzipay'
 
-import { Redis } from '../startup'
 import { User } from '../models'
 import { validateAuthority } from '../middlewares/auth-middleware'
 import Authority from '../enums/authority-enum'
 import ServerError from '../errors/ServerError'
-import { validateProducts, comparePasswords } from './validator'
+import { comparePasswords } from './validator'
 import ErrorMessages from '../errors/ErrorMessages'
 
-const router = Router()
+import {
+	listCards,
+	addCardToUser,
+	updateUser,
+	saveCart,
+	makeOrder,
+	deleteAddress
+} from '../services/user'
 
-const iyzipay = new Iyzipay({
-	apiKey: 'sandbox-hbjzTU7CZDxarIUKVMhWLvHOIMIb3Z40',
-	secretKey: 'sandbox-F01xQT4VMHAdFDB4RFNgo2l0kMImhCPX',
-	uri: 'https://sandbox-api.iyzipay.com'
-})
+const router = Router()
 
 router.use(validateAuthority(Authority.USER))
 
 router.get('/list-cards', (req, res, next) => {
-	iyzipay.cardList.retrieve({
-		locale: Iyzipay.LOCALE.TR,
-		conversationId: '123456789',
-		cardUserKey: 'ojBya0o8ecs7ynou3l94xmdB8f8='
-	}, (error: any, result: any) => {
-		if (error) {
-			next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, JSON.stringify(error), true))
-		} else {
-			res.json(result)
-		}
+	// listCards(req.body.cardUserKey)
+	listCards('ojBya0o8ecs7ynou3l94xmdB8f8=').then((cards) => {
+		res.json(cards)
+	}).catch((reason) => {
+		next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, reason.message, true))
 	})
 })
 
 router.post('/payment-card', (req, res, next) => {
-	iyzipay.card.create({
-		locale: Iyzipay.LOCALE.TR,
-		// conversationId: '123456789',
-		cardUserKey: 'ojBya0o8ecs7ynou3l94xmdB8f8=', // TODO get from user object from database/redis
-		card: req.body.card // Validate card datas	//	{
-		//		cardAlias: 'card alias',
-		//		cardHolderName: 'John Doe',
-		//		cardNumber: '5890040000000016',
-		//		expireMonth: '12',
-		//		expireYear: '2030'
-		//	}
-	}, (error: any, result: any) => {
-		if (error) {
-			next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, JSON.stringify(error), true))
-		} else {
-			res.json(result)
-		}
+	addCardToUser('ojBya0o8ecs7ynou3l94xmdB8f8=', req.body.card).then((result) => {
+		res.json(result)
+	}).catch((reason) => {
+		next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, reason.message, true))
 	})
 })
 
-router.put('/profile', (req, res) => {
+router.put('/profile', (req, res, next) => {
 	// @ts-ignore
-	User.findByIdAndUpdate(req.user._id, req.body).then((user) => {
+	updateUser(req.user._id, req.body).then((user) => {
 		res.json(user)
+	}).catch((reason) => {
+		next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, reason.message, true))
 	})
 })
 
 router.post('/cart', (req, res, next) => {
-	const { error } = validateProducts(Object.values(req.body))
-
-	if (!error) {
-		// @ts-ignore
-		Redis.getInstance.hset('cart', req.user._id.toString(), JSON.stringify(req.body), (redisError) => {
-			if (redisError) {
-				next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, redisError.message, true))
-			} else {
-				res.json({ status: true })
-			}
-		})
-	} else {
-		next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.BAD_REQUEST, JSON.stringify(error), true))
-	}
+	// @ts-ignore
+	saveCart(req.user._id.toString(), req.body).then((result) => {
+		res.json(result)
+	}).catch((reason) => {
+		next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, reason.httpCode ?? HttpStatusCodes.INTERNAL_SERVER_ERROR, reason.message, true))
+	})
 })
 
 router.get('/cart', (req, res, next) => {
 	//  @ts-ignore
-	// Redis.getInstance.hdel('cart', req.user._id.toString())
-	Redis.getInstance.hget('cart', req.user._id.toString(), (error, reply) => {
-		if (error) {
-			next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, error.message, true))
-		} else {
-			res.json(JSON.parse(reply))
-		}
+	getCart(req.user._id.toString()).then((result) => {
+		res.json(result)
+	}).catch((reason: any) => {
+		next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, reason.httpCode ?? HttpStatusCodes.INTERNAL_SERVER_ERROR, reason.message, true))
 	})
 })
 
@@ -109,68 +83,26 @@ router.post('/address', (req, res, next) => {
 
 router.delete('/address/:id', (req, res, next) => {
 	// @ts-ignore
-	User.findById(req.user._id).then((user: any) => {
-		if (user) {
-			const deletedAddress = user.addresses.indexOf(user.addresses.find((address: any) => address._id.toString() === req.params.id))
-			if (deletedAddress !== -1) {
-				user.addresses.splice(deletedAddress, 1)
-				user.save().then((result: any) => {
-					res.json(result)
-				})
-			} else {
-				next(new ServerError(ErrorMessages.NO_ADDRESS, HttpStatusCodes.BAD_REQUEST, 'DELETE /user/address', false))
-			}
-		} else {
-			next(new ServerError(ErrorMessages.USER_IS_NOT_EXISTS, HttpStatusCodes.BAD_REQUEST, 'DELETE /user/address', true))
-		}
+	deleteAddress(req.user._id, req.params.id).then((result) => {
+		res.json(result)
 	}).catch((reason) => {
-		next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.BAD_REQUEST, reason.message, true))
+		if (reason.httpCode) {
+			next(reason)
+		} else {
+			next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, reason.message, true))
+		}
 	})
 })
 
 router.post('/order', (req, res, next) => {
 	// @ts-ignore
-	const selecetedAddress = req.user.addresses.find((address) => address._id.toString() === req.body.address)
-
-	// @ts-ignore
-	Redis.getInstance.hget('cart', req.user._id.toString(), (getErr, cart) => {
-		if (!cart) {
-			next(new ServerError(ErrorMessages.EMPTY_CART, HttpStatusCodes.BAD_REQUEST, 'POST /user/order', false))
-			// @ts-ignore
-		} else if (!selecetedAddress) {
-			next(new ServerError(ErrorMessages.NO_ADDRESS, HttpStatusCodes.BAD_REQUEST, 'POST /user/order', false))
-		} else if (!getErr) {
-			const id = Math.random().toString()
-
-			// @ts-ignore
-			const val = {
-				id,
-				// @ts-ignore
-				customer: req.user.name_surname,
-				// @ts-ignore
-				address: selecetedAddress.open_address,
-				date: new Date().toLocaleString(),
-				// starts : 2.2 // Müşteri daha önce memnuniyetsizliğini belirttiyse bi güzellik yapılabilir. :)
-				// price: (23.43 * 5) + (76.36 * 2), // Online ödemelerde manager'ın ücret ile işi yok.
-				products: Object.values(cart)
-			}
-
-			const multi = Redis.getInstance.multi()
-
-			multi.hset('category1', id, JSON.stringify(val))
-
-			// @ts-ignore
-			multi.hdel('cart', req.user._id.toString())
-
-			multi.exec((error) => {
-				if (error) {
-					next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, error.message, true))
-				} else {
-					res.json({ status: true })
-				}
-			})
+	makeOrder(req.user._id.toString(), req.body).then((result) => {
+		res.json(result)
+	}).catch((reason) => {
+		if (reason.httpCode) {
+			next(reason)
 		} else {
-			next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, getErr.message, true))
+			next(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, reason.message, true))
 		}
 	})
 })
---
+++ src/services/user.ts
@@ -0,0 +1,157 @@
+import HttpStatusCodes from 'http-status-codes'
+import Iyzipay from 'iyzipay'
+
+import ErrorMessages from '../errors/ErrorMessages'
+import ServerError from '../errors/ServerError'
+import { User } from '../models'
+import { validateProducts } from '../controllers/validator'
+import { Redis } from '../startup'
+
+const iyzipay = new Iyzipay({
+	apiKey: 'sandbox-hbjzTU7CZDxarIUKVMhWLvHOIMIb3Z40',
+	secretKey: 'sandbox-F01xQT4VMHAdFDB4RFNgo2l0kMImhCPX',
+	uri: 'https://sandbox-api.iyzipay.com'
+})
+
+export const listCards = (cardUserKey: string) => (
+	new Promise((resolve, reject) => {
+		iyzipay.cardList.retrieve({
+			locale: Iyzipay.LOCALE.TR,
+			conversationId: '123456789',
+			cardUserKey
+		}, (error: any, cards: any) => {
+			if (error) {
+				reject(new Error(JSON.stringify(error)))
+			} else {
+				resolve(cards)
+			}
+		})
+	})
+)
+
+export const addCardToUser = (cardUserKey: string, card: any) => (
+	new Promise((resolve, reject) => {
+		iyzipay.card.create({
+			locale: Iyzipay.LOCALE.TR,
+			// conversationId: '123456789',
+			cardUserKey, // TODO get from user object from database/redis
+			card// Validate card datas	//	{
+			//		cardAlias: 'card alias',
+			//		cardHolderName: 'John Doe',
+			//		cardNumber: '5890040000000016',
+			//		expireMonth: '12',
+			//		expireYear: '2030'
+			//	}
+		}, (error: any, result: any) => {
+			if (error) {
+				reject(new Error(JSON.stringify(error)))
+			} else {
+				resolve(result)
+			}
+		})
+	})
+)
+
+export const updateUser = (userId: string, userContext: any) => (
+	User.findByIdAndUpdate(userId, userContext)
+)
+
+export const saveCart = (userId: string, cart: any) => (
+	new Promise((resolve, reject) => {
+		const { error } = validateProducts(Object.values(cart))
+
+		if (!error) {
+			// @ts-ignore
+			Redis.getInstance.hset('cart', userId, JSON.stringify(cart), (redisError) => {
+				if (redisError) {
+					reject(new Error(redisError.message))
+				} else {
+					resolve({ status: true })
+				}
+			})
+		} else {
+			reject(new Error(JSON.stringify(error)))
+		}
+	})
+)
+
+export const getCart = (userId: string) => (
+	new Promise((resolve, reject) => {
+		// Redis.getInstance.hdel('cart', req.user._id.toString())
+		Redis.getInstance.hget('cart', userId, (error, reply) => {
+			if (error) {
+				reject(new Error(error.message))
+			} else {
+				resolve(JSON.parse(reply))
+			}
+		})
+	})
+)
+
+export const deleteAddress = (userId: string, deletedAddressId: string) => (
+	// @ts-ignore
+	User.findById(userId).then((user: any) => {
+		if (!user) {
+			throw new ServerError(ErrorMessages.USER_IS_NOT_EXISTS, HttpStatusCodes.BAD_REQUEST, 'DELETE /user/address', true)
+		} else {
+			const deletedAddress = user.addresses.indexOf(user.addresses.find((address: any) => address._id.toString() === deletedAddressId))
+			if (deletedAddress === -1) {
+				throw new ServerError(ErrorMessages.NO_ADDRESS, HttpStatusCodes.BAD_REQUEST, 'DELETE /user/address', false)
+			} else {
+				user.addresses.splice(deletedAddress, 1)
+				return user.save()
+			}
+		}
+	}).catch((reason) => {
+		throw new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.BAD_REQUEST, reason.message, true)
+	})
+)
+
+export const makeOrder = (user: any, context: any) => (
+	new Promise((resolve, reject) => {
+		// @ts-ignore
+		const selecetedAddress = user.addresses.find((address) => address._id.toString() === context.address)
+
+		// @ts-ignore
+		Redis.getInstance.hget('cart', user._id.toString(), (getErr, cart) => {
+			if (!cart) {
+				reject(new ServerError(ErrorMessages.EMPTY_CART, HttpStatusCodes.BAD_REQUEST, 'POST /user/order', false))
+				// @ts-ignore
+			} else if (!selecetedAddress) {
+				reject(new ServerError(ErrorMessages.NO_ADDRESS, HttpStatusCodes.BAD_REQUEST, 'POST /user/order', false))
+			} else if (!getErr) {
+				const id = Math.random().toString()
+
+				// @ts-ignore
+				const val = {
+					id,
+					// @ts-ignore
+					customer: user.name_surname,
+					// @ts-ignore
+					address: selecetedAddress.open_address,
+					date: new Date().toLocaleString(),
+					// starts : 2.2 // Müşteri daha önce memnuniyetsizliğini belirttiyse bi güzellik yapılabilir. :)
+					// price: (23.43 * 5) + (76.36 * 2), // Online ödemelerde manager'ın ücret ile işi yok.
+					products: Object.values(cart)
+				}
+
+				const multi = Redis.getInstance.multi()
+
+				multi.hset('category1', id, JSON.stringify(val))
+
+				// @ts-ignore
+				multi.hdel('cart', user._id.toString())
+
+				multi.exec((error) => {
+					if (error) {
+						reject(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, error.message, true))
+					} else {
+						resolve({ status: true })
+					}
+				})
+			} else {
+				reject(new ServerError(ErrorMessages.UNEXPECTED_ERROR, HttpStatusCodes.INTERNAL_SERVER_ERROR, getErr.message, true))
+			}
+		})
+	})
+)
